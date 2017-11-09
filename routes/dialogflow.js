fd71f6ae312a2d21b20a6a84f33d80a330268d85
Michael Schouten <michael@tambien.nl>
2017-11-09 09:59:16 +0100

    Console logs for testing added

--- routes/dialogflow.js
+++ routes/dialogflow.js
@@ -2,11 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Dialogflow' });
-});
+// router.get('/', function (req, res, next) {
+//   res.render('index', { title: 'Dialogflow' });
+// });
 
 router.post('/', function (req, res, next) {
+	console.log("Req: " + req);
 	var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem occured. Speak again."
 	return res.json({
 		speech: speech,
